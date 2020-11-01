7754fcf7dc4b458931972e1e7eb5932a7ba887a6
Ishaan Verma <ishaan56verma@gmail.com>
2020-11-01 18:39:12 +0000

    change port to 1337

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ from app import app
 import os
 
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000 if os.getenv("PORT") is None else int(os.getenv("PORT")),'0.0.0.0')
+http_server.listen(1337 if os.getenv("PORT") is None else int(os.getenv("PORT")),'0.0.0.0')
 IOLoop.instance().start()
